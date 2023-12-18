4e8a28134c8340541a323a6e5d130585d937a58b
Ibnu Habibie <m.habibieibnu@gmail.com>
2023-12-18 14:41:29 +0700

    add basic socketio

--- app.js
+++ app.js
@@ -1,10 +1,36 @@
 const express = require('express');
+const { join } = require('node:path');
+const { Server } = require('socket.io');
 require('dotenv').config();
 
 const app = express();
-const { PORT } = process.env;
-
 app.use(express.static('public'));
+
+const { PORT, SOCKET_PORT } = process.env;
+
+const io = new Server(SOCKET_PORT, {
+  cors: {
+    origin: '*',
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected', socket.id);
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+
+  setInterval(() => {
+    io.emit('message', {
+      date: new Date(),
+      id: (Math.random() + 1).toString(36).substring(7),
+    });
+  }, 10000);
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(join(__dirname, 'index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(
---
+++ public/js/main.js
@@ -0,0 +1,43 @@
+Audio.prototype.play = (function (play) {
+  return function () {
+    var audio = this,
+      args = arguments,
+      promise = play.apply(audio, args);
+    if (promise !== undefined) {
+      promise.catch((_) => {
+        console.log('autoplay denied');
+      });
+    }
+  };
+})(Audio.prototype.play);
+
+const socket = io(`${window.origin}:3030`);
+
+socket.on('message', async (msg) => {
+  let notif = document.getElementById('audio');
+  notif.play();
+  // let permission = await Notification.requestPermission();
+  // if (permission == 'granted') {
+  // const greeting = new Notification('Hi, How are you?');
+  // greeting.onclick = () => window.open('http://localhost:3000/');
+  // }
+  // console.log(permission);
+
+  let li = document.createElement('li');
+  let id = document.createElement('span');
+  id.innerText = msg.id;
+
+  let date = document.createElement('span');
+  date.innerText = msg.date;
+
+  li.appendChild(id);
+  li.appendChild(date);
+
+  document.getElementById('content').appendChild(li);
+});
+
+let btnClear = document.querySelector('button');
+btnClear.addEventListener('click', function () {
+  let content = document.getElementById('content');
+  content.innerHTML = '';
+});
